70d0273800242d5a9ce47e0a164a90c53ba9434c
akshmohan <145959795+akshmohan@users.noreply.github.com>
2024-10-03 13:46:35 +0530

    Created Sign Up API implemented the feature in the Sign Up Page

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import userRouter from './routes/user.routes.js';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 mongoose.connect("mongodb+srv://akshaymohancareer:wTtZdcZJv88RDfD6@mern-estate.zdhny.mongodb.net/mern-estate?retryWrites=true&w=majority&appName=mern-estate").then(()=>{
     console.log("Connected to MongoDB");
@@ -9,9 +10,23 @@ mongoose.connect("mongodb+srv://akshaymohancareer:wTtZdcZJv88RDfD6@mern-estate.z
 });
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000,() =>{
 console.log("Server is running on port 3000!!!")
 }); 
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+
+//MIDDLEWARE
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    });
+});
