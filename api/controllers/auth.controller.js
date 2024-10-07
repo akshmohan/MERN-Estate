495d373d628570114d1f8aa75ef09bbd036811f7
akshmohan <145959795+akshmohan@users.noreply.github.com>
2024-10-07 10:12:16 +0530

    Created the sign in function successfully, got the 200 response for the sign in api in Thunder Client

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
-// import { errorHandler } from '../utils/error.js';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
+
 
 export const signup = async(req, res, next) => {
 
@@ -14,3 +16,25 @@ export const signup = async(req, res, next) => {
     next(error);
   }
 }
+
+export const signin = async (req, res, next) => {
+  
+  const {email, password} = req.body;
+
+  try {
+      
+    const validUser = await User.findOne({email});
+    if(!validUser) return next(errorHandler(404, "User not found"));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if(!validPassword) return next(errorHandler(401, "Invalid credentials"));
+
+    const token = jwt.sign({id: validUser._id}, "coding is fun");
+
+    res.cookie('access token', token, {httpOnly: true}).status(200).json({message: "User logged in Successfully"});
+
+  } catch (error) {
+    next(error);
+  }
+
+};
